8a4c492e4fc7e1b6db261411322ce60f3133fd75
sole <listas@soledadpenades.com>
2014-10-04 11:39:09 +0100

    hapi version... maybe

--- src/server.js
+++ src/server.js
@@ -1,131 +1,64 @@
-var http = require('http');
-var Nconf = require('nconf');
-var Express = require('express');
-var ExpressSession = require('express-session');
-var ExpressMethodOverride = require('express-method-override');
-var CookieParser = require('cookie-parser');
-var Passport = require('passport');
-var TwitterStrategy = require('passport-twitter').Strategy;
-//var User = require('./User');
+var Hapi = require('hapi');
+var nconf = require('nconf');
+var server = new Hapi.Server(3000);
+
+/*server.start(function () {
+	console.log('Server running at:', server.info.uri);
+});*/
+
+nconf.argv().env().file({ file: 'settings-local.json' });
+
+var TWITTER_CONSUMER_KEY = nconf.get('twitter_consumer_key');
+var TWITTER_CONSUMER_SECRET = nconf.get('twitter_consumer_secret');
+var COOKIE_ENCRYPTION_PASSWORD = 'whaaaateveeer,changethis!!!';
 
 
-Nconf.argv().env().file({ file: 'settings-local.json' });
-
-var TWITTER_CONSUMER_KEY = Nconf.get('twitter_consumer_key');
-var TWITTER_CONSUMER_SECRET = Nconf.get('twitter_consumer_secret');
-
-var app = Express();
-
-app.use(CookieParser());
-app.use(ExpressMethodOverride());
-app.use(ExpressSession({
-	secret: Nconf.get('session_secret'),
-	saveUninitialized: true,
-	resave: true
-}));
-
-app.use(Passport.initialize());
-app.use(Passport.session());
-
-var server = app.listen(3000, '127.0.0.1', 511, onServerListening);
-
-
-app.get('/', function(req, res) {
-	if(req.user) {
-		console.log('a user is logged in', req.user.name);
-		res.send('hey ' + req.user.name + ' <a href="/logout">logout</a>');
-	} else {
-		console.log('not logged');
-		res.send('heeey <a href="/login">login</a>');
+server.route({
+	method: ['GET'],
+	path: '/',
+	handler: function(request, reply) {
+		console.log(request.auth);
+		if(request.auth.credentials) {
+			reply('wow you are so authenticated');
+		} else {
+			reply('hullo <a href="/login">login please</a>');
+		}
 	}
 });
 
-
-function onServerListening() {
-	console.log('Listening on %d', server.address().port);
-	console.log(server.address());
-	setupTwitter();
-}
-
-// The twitterama ~~~~
-
-var users = {}; // XXX FIXME use a proper database lol
-
-function findUser(uid, callback) {
-	var user = users[uid];
-	callback(false, user);
-}
-
-function setupTwitter() {
-
-	console.log('twitterarama!');
-
-	var addr = server.address();
-
-	// uhrrhghhg this is awful XXX FIXME FIXALLTHETHINGS
-	var TWITTER_AUTH_PATH = '/auth/twitter';
-	var TWITTER_AUTH_CALLBACK_PATH = '/auth/twitter/callback';
-	var TWITTER_CALLBACK_URL = 'http://' + addr.address + ':' + addr.port + TWITTER_AUTH_CALLBACK_PATH;
-
-
-	app.get('/login', function(req, res) {
-		res.send('<a href="' + TWITTER_AUTH_PATH + '">login with twitter</a>');
+server.pack.register(require('bell'), function (err) {
+	
+	server.auth.strategy('twitter', 'bell', {
+		provider: 'twitter',
+		password: 'cookie_encryption_password',
+		clientId: TWITTER_CONSUMER_KEY,
+		clientSecret: TWITTER_CONSUMER_SECRET,
+		isSecure: false     // TODO change to true if using https in server proto
 	});
 
-	Passport.use(new TwitterStrategy({
-			consumerKey: TWITTER_CONSUMER_KEY,
-			consumerSecret: TWITTER_CONSUMER_SECRET,
-			callbackURL: TWITTER_CALLBACK_URL
-		},
-		function(token, tokenSecret, profile, done) {
-			console.log(token, tokenSecret, profile);
-			
-			//User.findOrCreate(..., function(err, user) {
-			//	if (err) { return done(err); }
-			//	done(null, user);
-			//});
-			
-			if(profile) {
-				var user = {
-					id: profile.id,
-					name: profile.username,
-					fullname: profile.displayName
-				};
 
-				var uid = user.id;
+	server.route({
+		method: ['GET', 'POST'], // Must handle both GET and POST
+		path: '/login',          // The callback endpoint registered with the provider
+		config: {
+			auth: {
+				strategy: 'twitter',
+				mode: 'try'
+			},
+			handler: function (request, reply) {
 
-				// TODO create own id, don't rely on twitter's
-				if(!users[uid]) {
-					users[uid] = user;
+				// Perform any account lookup or registration, setup local session,
+				// and redirect to the application. The third-party credentials are
+				// stored in request.auth.credentials. Any query parameters from
+				// the initial request are passed back via request.auth.credentials.query.
+				
+				if (!request.auth.isAuthenticated) {
+					return reply('Authentication failed due to: ' + request.auth.error.message);
 				}
-
-				done(null, user);
-			} else {
-				done(err);
+				return reply.redirect('/');
 			}
-
 		}
-	));
-
-
-
-	app.get(TWITTER_AUTH_PATH, Passport.authenticate('twitter'));
-
-	app.get(TWITTER_AUTH_CALLBACK_PATH,
-		Passport.authenticate('twitter', { successRedirect: '/',
-			failureRedirect: '/login'
-	}));
-
-	Passport.serializeUser(function(user, done) {
-		console.log('serialize user', user);
-		done(null, user.id);
 	});
 
-	Passport.deserializeUser(function(uid, done) {
-		console.log('deserialize user', uid);
-		findUser(uid, function (err, user) {
-			done(err, user);
-		});
-	});
-}
-
+	server.start(function() { console.log('si', server.info.uri); });
+});
